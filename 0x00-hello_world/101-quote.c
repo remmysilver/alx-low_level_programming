2d7aadfd084bd6950be50775c8d094b12013bcd5
remmysilver <silverremmy@gmail.com>
2022-09-08 16:19:37 -0700

    --

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  *
  * main - print quote
@@ -13,13 +12,10 @@
  *
  * Return: Always 1 (Success)
  *
- **/
+ */
 int main(void)
 
 {
-
         write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
 	return (1);
-
 }
