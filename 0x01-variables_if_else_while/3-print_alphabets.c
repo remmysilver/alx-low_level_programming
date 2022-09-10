1958d7d740b26358041fadd8e99b3c84216dd7c3
remmysilver <silverremmy@gmail.com>
2022-09-09 18:29:56 -0700

    numbers and alphabets using if-else-for-while statements

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - prints the alphabet in lowercase, and then in uppercase,
+ *followed by a new line
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+		int n;
+
+
+
+			srand(time(0));
+
+				n = rand() - RAND_MAX / 2;
+
+					if (n == 0)
+						printf("%d is zero\n", n);
+					else if (n < 0)
+						printf("%d is negative\n", n);
+					else
+						printf("%d is positive\n", n);
+					return (0);
+
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+conditions-if-else
