d125181b9afa79e7ab9fb4f90c77ea794fe662b7
remmysilver <silverremmy@gmail.com>
2022-09-14 14:36:46 -0700

    a function that prints the 9 times table, starting with 0 a function that prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *times_table - prints the 9 times table
  *Description: prints the 9 times table
@@ -25,7 +26,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(ones + 'o');
+				_putchar(ones + '0');
 			}
 			else
 			{
