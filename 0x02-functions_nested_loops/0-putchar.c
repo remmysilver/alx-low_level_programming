6d14da6f9761d3dbd43b5bf930bdeb87c4ea4b89
remmysilver <silverremmy@gmail.com>
2022-09-14 12:27:39 -0700

    for loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - prints putchar
+ *Description: prints  putchar
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char text[8] = "_putchar";
+	int  i = 0;
+
+	for (i = 0; i < 7; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
