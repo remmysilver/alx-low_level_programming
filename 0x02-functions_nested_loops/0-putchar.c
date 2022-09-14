6561fad9681a5a2068457b6eb5e621efa825bbec
remmysilver <silverremmy@gmail.com>
2022-09-14 13:00:50 -0700

    --

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,15 @@
 
 int main(void)
 {
-	char text[8] = "_putchar";
-	int  i = 0;
-
-	for (i = 0; i < 7; i++)
-	{
-		_putchar(text[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
