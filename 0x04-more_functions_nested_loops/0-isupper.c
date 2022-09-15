7c9acd002eebcab9e0cae70a2c986ad471cefd6f
remmysilver <silverremmy@gmail.com>
2022-09-15 11:33:25 -0700

    uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,5 +10,11 @@
 
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z')
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
 }
