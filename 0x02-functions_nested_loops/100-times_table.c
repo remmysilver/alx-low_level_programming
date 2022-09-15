744eb7e38a68bf1c1977b330d5fdd608b5855d99
remmysilver <silverremmy@gmail.com>
2022-09-14 17:59:28 -0700

    --

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,44 +6,44 @@
 
 void print_times_table(int n)
 {
-	int i, j, k;
+	int row, column, product;
 
-	if (n >= 0 && n <= 15)
+	if (n >= 0 && n < 15)
 	{
+		for (row = 0; row <= n; row++)
 		{
-			for (i = 0; i <= n; i++)
+			for (column = 0; column <= n; column++)
 			{
-				for (j = 0; j <= n; j++)
-					{
-						k = j * i;
-						if (j == 0)
-						{
-							_putchar(k + '0');
-						} else if (k < 10 && j != 0)
-						{
-							_putchar(',');
-							_putchar(' ');
-							_putchar(' ');
-							_putchar(' ');
-							_putchar(k + '0');
-						} else if (k >= 10 && k < 100)
-						{
-							_putchar(',');
-							_putchar(' ');
-							_putchar(' ');
-							_putchar((k / 10) + '0');
-							_putchar((k % 10) + '0');
-						} else if (k >= 100)
-						{
-							_putchar(',');
-							_putchar(' ');
-							_putchar((k / 100) + '0');
-							_putchar(((k / 10) % 10) + '0');
-							_putchar((k % 10) + '0');
-						}
-					}
-					_putchar('\n');
+				product = row * column;
+
+				if (column == 0)
+					_putchar('0');
+				else if (product < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product % 10 + '0');
+				}
+				else if (product >= 10 && product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				else if (product > 99 && product < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(product / 100 + '0');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
 			}
+			_putchar('\n');
 		}
 	}
 }
