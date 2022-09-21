43fe5f4dc3b002f7e94a356b97dc81d7c12c2014
remmysilver <silverremmy@gmail.com>
2022-09-21 12:13:27 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,26 @@
 #include "main.h"
+
+/**
+ *leet - function that encodes a string into 1337
+ *@s: string array
+ *Return: string array
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+	return (s);
+}
