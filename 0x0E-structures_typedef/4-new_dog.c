c7efe520b8175c4d5020b0119898687d41df078f
remmysilver <silverremmy@gmail.com>
2022-10-11 07:21:46 -0700

    function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,6 +9,7 @@
 int _strlen(char *s)
 {
 	int i;
+
 	i = 0;
 
 	while (s[i] != '\0')
@@ -29,6 +30,7 @@ int _strlen(char *s)
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
+
 	len = 0;
 
 	while (src[len] != '\0')
