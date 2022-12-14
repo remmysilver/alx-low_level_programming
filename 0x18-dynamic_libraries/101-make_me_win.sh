deb245e315f08e122996e8c3c04a066dff8a6bbb
remmysilver <silverremmy@gmail.com>
2022-12-14 06:36:59 -0800

    make win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/betascribbles/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P /tmp https://github.com/remmysilver/alx-low_level_programming/blob/650603c7e1bbe3087290cb79717d15e3c3ee35f8/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/iwin.so
